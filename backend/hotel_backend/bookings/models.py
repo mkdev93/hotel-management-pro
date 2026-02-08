4eec38a7bd65bb8de1903ee9dce941c6de8de278
mkdev93 <developer.app.dz@gmail.com>
2026-02-08 22:14:52 +0100

    add logic to dates

--- backend/hotel_backend/bookings/models.py
+++ backend/hotel_backend/bookings/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from rooms.models import Room
+from django.core.exceptions import ValidationError # لاستخدامها في "صافرة الإنذار"
+from django.utils import timezone
 # Create your models here.
 class Guest(models.Model):
     # قائمة أنواع الهوية
@@ -14,6 +16,7 @@ class Guest(models.Model):
     date_of_birth = models.DateField(verbose_name="تاريخ الميلاد")
     place_of_birth = models.CharField(max_length=100, verbose_name="مكان الميلاد")
     address = models.TextField(verbose_name="عنوان السكن")
+    nationality = models.CharField(max_length=50, default='جزائري', verbose_name="الجنسية")
     phone_number = models.CharField(max_length=20, verbose_name="رقم الهاتف")
     # بيانات الهوية
     id_type = models.CharField(
@@ -27,6 +30,17 @@ class Guest(models.Model):
     id_issuing_authority = models.CharField(max_length=100, verbose_name="السلطة الصادرة للهوية")
     # التوقيت
     created_at = models.DateTimeField(auto_now_add=True)
+    def clean(self):
+        today = timezone.now().date()
+        if self.id_issue_date and self.id_issue_date > today:
+            raise ValidationError("تاريخ إصدار الهوية يجب أن يكون في الماضي.")
+        limit_date= today.replace(year=today.year - 18)
+        if self.date_of_birth > limit_date:
+            raise ValidationError("يجب أن يكون عمر النزيل 18 سنة على الأقل لإتمام الحجز.")
+
+    def save(self, *args, **kwargs):
+        self.clean() # استدعاء الحارس قبل الحفظ
+        super().save(*args, **kwargs)
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
     class Meta:
@@ -51,6 +65,23 @@ class Booking(models.Model):
     total_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="السعر الإجمالي")
     status = models.CharField(max_length=20, choices=BOOKING_STATUS, default='pending', verbose_name="حالة الحجز")
     created_at = models.DateTimeField(auto_now_add=True)
+    def clean(self):
+        today = timezone.now().date()
+        if self.check_in_date < today:
+            raise ValidationError("لا يمكن حجز الغرفة بتاريخ قديم، يجب أن يبدأ الحجز من اليوم فصاعداً.")
+        if self.check_out_date < self.check_in_date:
+            raise ValidationError("تاريخ الخروج يجب أن يكون بعد تاريخ الدخول (على الأقل ليلة واحدة).")
+        # ✅ التحقق من تداخل الحجوزات
+        overlapping_bookings = Booking.objects.filter(
+            room=self.room,
+            check_in_date__lt=self.check_out_date,
+            check_out_date__gt=self.check_in_date
+        ).exclude(pk=self.pk)
+
+        if overlapping_bookings.exists():
+            raise ValidationError("هذه الغرفة محجوزة في هذا التاريخ.")
+
+        
     def __str__(self):
         return f"حجز {self.guest} - غرفة {self.room.room_number}"
     class Meta:
